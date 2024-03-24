b9b2c1763671bd6c2e5108ef2640140e439b759a
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2024-03-25 01:49:40 +0330

    init go mod , add config loader

--- main.go
+++ main.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	"web-api/config"
-	"web-api/delivery/httpserver"
+	"newsapp/config"
 )
 
 func main() {
 	//get config
 	cfg := config.GetConfig()
 	fmt.Println(cfg)
-
-	server := httpserver.New(cfg)
-	go func() {
-		server.Serve()
-	}()
 
 }
