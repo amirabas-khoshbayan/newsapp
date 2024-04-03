747cced7325a19604e40ee089f94cce7b4d4601b
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2024-04-03 21:23:59 +0330

    fix issue unknown colum first_name at insert user , get user by phone number

--- repository/mysql/mysqluser/user.go
+++ repository/mysql/mysqluser/user.go
@@ -19,7 +19,7 @@ func (d *DB) GetUserByID(ctx context.Context, userID string) (entity.User, error
 	return user, nil
 }
 func (d *DB) InsertUser(user entity.User) (entity.User, error) {
-	result, err := d.userConn.Conn().Exec(`INSERT INTO user(firstname, lastname, phone_number,password,role, email) VALUES(?,?,?,?,?,?) `,
+	result, err := d.userConn.Conn().Exec(`INSERT INTO user(first_name, last_name, phone_number,password,role, email) VALUES(?,?,?,?,?,?) `,
 		user.FirstName, user.LastName, user.PhoneNumber, user.Password, user.Role, user.Email)
 	if err != nil {
 
@@ -33,8 +33,17 @@ func (d *DB) InsertUser(user entity.User) (entity.User, error) {
 }
 
 func (d *DB) GetUserByPhoneNumber(ctx context.Context, phoneNumber string) (entity.User, error) {
-	//TODO implement me
-	panic("implement me")
+	rows, err := d.userConn.Conn().QueryContext(ctx, ` SELECT * FROM user WHERE phone_number = ?`, phoneNumber)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	user, err := scanUser(rows)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return user, nil
 }
 
 func (d *DB) GetUsers(ctx context.Context) ([]entity.User, error) {
