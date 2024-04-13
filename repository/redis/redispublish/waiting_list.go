e625e26d46dd272729ac5f7048c357fa0260e0b9
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2024-04-13 06:36:33 +0330

    get waiting list news by category

--- repository/redis/redispublish/waiting_list.go
+++ repository/redis/redispublish/waiting_list.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
 	"newsapp/entity"
+	"strconv"
 	"time"
 )
+
+func getCategoryKey(category entity.Category) string {
+	return fmt.Sprintf("%s:%s", "waitinglist", category)
+}
 
 func nowTime() int64 {
 	return time.Now().UnixMicro()
@@ -14,7 +19,7 @@ func nowTime() int64 {
 
 func (d DB) AddNewsToWaitingList(ctx context.Context, newsID uint, category entity.Category) error {
 
-	_, err := d.adapter.Client().ZAdd(ctx, fmt.Sprintf("%s:%s", "waitinglist", category),
+	_, err := d.adapter.Client().ZAdd(ctx, getCategoryKey(category),
 		redis.Z{
 			Score: float64(nowTime()), Member: fmt.Sprintf("%d", newsID)}).Result()
 	if err != nil {
@@ -25,8 +30,32 @@ func (d DB) AddNewsToWaitingList(ctx context.Context, newsID uint, category enti
 }
 
 func (d DB) GetWaitingListNewsByCategory(ctx context.Context, category entity.Category) ([]entity.WaitingNews, error) {
-	//TODO implement me
-	panic("implement me")
+
+	min := fmt.Sprintf("%d", time.Now().Add(-2*time.Hour).UnixMicro())
+	max := strconv.Itoa(int(nowTime()))
+	list, err := d.adapter.Client().ZRangeByScoreWithScores(ctx, getCategoryKey(category), &redis.ZRangeBy{
+		Min:    min,
+		Max:    max,
+		Offset: 0,
+		Count:  0,
+	}).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	var reult = make([]entity.WaitingNews, 0)
+
+	for _, l := range list {
+		newsID, _ := strconv.Atoi(l.Member.(string))
+
+		reult = append(reult, entity.WaitingNews{
+			NewsID:    uint(newsID),
+			Timestamp: int64(l.Score),
+			Category:  category,
+		})
+	}
+
+	return reult, nil
 }
 
 func (d DB) RemoveNewsFromWaitingList(category entity.Category, newsIDs []uint) {
